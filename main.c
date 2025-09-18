06cf0ec55ff3eca838dd0fababb7b8fcf5a7195d
helloyewon36 <helloyewon36@gmail.com>
2025-09-18 16:49:27 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,15 +2,18 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    char c;
+    int a,b;
     
-    printf("Enter a character: ");
-    scanf("%c", &c);
+    printf("enter bunja:");
+    scanf("%d", &a);
     
-    printf("The next character of %c (%1) is %c (%i)\n", c, c, c+1, c+1);
+    printf("enter bunmo:");
+    scanf("%d", &b);
     
-    system("PAUSE");
+    printf("The result is %f\n", (float)a/b);
+    
     return 0;
+
 }
     
 
